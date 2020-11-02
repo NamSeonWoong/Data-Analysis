567c3b3cd6baef831424930a6d1aaf9b4673b10b
남선웅 <wun1107@gmail.com>
2020-11-02 18:42:26 +0900

    그래프 타이틀, input 수정

--- 인구데이터 분석.py
+++ 인구데이터 분석.py
@@ -4,14 +4,17 @@ import matplotlib.pyplot as plt
 
 f= open('age.csv')
 data= csv.reader(f)
+name=input('알고싶은 지역을 입력하세요 :')
 result=[]
 
 for row in data:
     if '북구 우산동' in row[0]:
         for i in row[3:]:
-            result.append(int(i))
+            result.append(int(i.replace(',','')))
 print(result)
 
 plt.style.use('ggplot')
+plt.rc('font',family='Malgun Gothic')
+plt.title(name+'지역의 인구 구조')
 plt.plot(result)
 plt.show()
